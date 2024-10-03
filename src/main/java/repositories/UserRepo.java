a4c775c4f1456855970f31f424d416c3a466ca1f
Crewcut93 <crew-cut93@yandex.by>
2024-10-04 01:47:41 +0300

    start
    (cherry picked from commit 512441bb4c2ecd2883d3bdcf7e3418876114255d)

--- src/main/java/repositories/UserRepo.java
+++ src/main/java/repositories/UserRepo.java
@@ -1,11 +1,12 @@
 package repositories;
 
 import domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface UserRepo extends CrudRepository<User, Integer> {
+public interface UserRepo extends JpaRepository <User, Long> {
     List<User> findByName(String name);
 
 
